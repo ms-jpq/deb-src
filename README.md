28db4cad88079abbdd4a5032411a2e50dbb2cbeb
root <github@bigly.dog>
2024-03-17 21:23:30 -0700

    readme

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # APT
 
 ```bash
-source -- /etc/os-release
-tee -- /etc/apt/sources.list.d/ms-jpq.list <<-EOF
-deb https://ms-jpq.github.io/deb-src/\$(ARCH) /
+tee -- /etc/apt/sources.list.d/ms-jpq.list <<-'EOF'
+deb https://ms-jpq.github.io/deb/ /
 EOF
 ```
--- makelib/pkg.mk
+++ makelib/pkg.mk
@@ -1,8 +1,8 @@
 .PHONY: pkg
 
-/usr/bin/dpkg-scanpackages:
+/usr/bin/dpkg-sig /usr/bin/apt-ftparchive /usr/bin/dpkg-scanpackages:
 	sudo -- apt-get update
-	sudo -- env -- DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends --yes -- dpkg-dev
+	sudo -- env -- DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends --yes -- apt-utils dpkg-sig
 
 $(DIST)/Packages: $(PKGS) | /usr/bin/dpkg-scanpackages $(DIST)
 	env --chdir '$(@D)' -- dpkg-scanpackages --multiversion -- . >'$@'
@@ -11,6 +11,9 @@ pkg: $(DIST)/Packages.gz
 $(DIST)/Packages.gz: $(DIST)/Packages
 	gzip --keep --no-name --force -- '$<'
 
+$(DIST)/Release: $(DIST)/Packages | /usr/bin/apt-ftparchive $(DIST)
+	env --chdir '$(@D)' -- apt-ftparchive release . >'$@'
+
 pkg: $(DIST)/InRelease
-$(DIST)/InRelease: $(DIST)/Packages
-	gpg --clearsign --output '$@' -- '$<'
+$(DIST)/InRelease: $(DIST)/Release
+	gpg --batch --clearsign --yes --output '$@' -- '$<'
