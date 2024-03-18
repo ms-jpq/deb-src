bb974ea4fc8a2c8a9cf09c1d6f26905418a95afa
root <github@bigly.dog>
2024-03-18 12:46:05 -0700

    k

--- makelib/packages.mk
+++ makelib/packages.mk
@@ -84,9 +84,9 @@ $(V_S5CMD)   s5cmd          https://github.com/peak/s5cmd/releases/latest/downlo
 $(V_SAD)     sad            https://github.com/ms-jpq/sad/releases/latest/download/#{HOSTTYPE}-unknown-linux-gnu.deb                               %
 $(V_TV)      tidy-viewer    https://github.com/alexhallam/tv/releases/latest/download/tidy-viewer_#{VERSION}_#{GOARCH}.deb                         %aarch64=!
 $(V_WATCHEX) watchexec      https://github.com/watchexec/watchexec/releases/latest/download/watchexec-#{VERSION}-#{HOSTTYPE}-unknown-linux-gnu.deb %
-22.04        microsoft-list https://packages.microsoft.com/config/ubuntu/#{VERSION}/packages-microsoft-prod.deb                                    %
 
 endef
+# 22.04        microsoft-list https://packages.microsoft.com/config/ubuntu/#{VERSION}/packages-microsoft-prod.deb                                    %
 
 
 CURL_ARCHIVES := $(shell ./libexec/arch-tee.sh <<<'$(CURL_ARCHIVES)')
