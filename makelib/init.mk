cad171f339182d28836318336cd4f8246a310e4c
root <github@bigly.dog>
2024-03-18 13:27:47 -0700

    more parallel

--- makelib/init.mk
+++ makelib/init.mk
@@ -1,14 +1,10 @@
 GH_LATEST := ./libexec/gh-latest.sh $(TMP)
 CURL := curl --fail-with-body --location --no-progress-meter
 
-.PHONY: init
-
-init: /usr/bin/envsubst
 /usr/bin/envsubst /usr/bin/debsigs /usr/bin/apt-ftparchive:
 	sudo -- apt-get update
 	sudo -- env -- DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends --yes -- gettext-base apt-utils debsigs
 
-init: $(VAR)/sh
 $(VAR)/sh: | $(VAR)
 	if [[ -d '$@' ]]; then
 		git -C '$@' pull --recurse-submodules --no-tags
