4844b6306a2a5f9d459c9feea1038790e5e7d8e6
root <github@bigly.dog>
2024-03-19 20:32:02 -0700

    dat

--- makelib/pack-v2ray.mk
+++ makelib/pack-v2ray.mk
@@ -1,1 +1,17 @@
-# TODO: deb for geoip & geosite only
+V2RAY_NAME := all_$(V_V2RAY)_v2ray-dat
+
+$(TMP)/$(V2RAY_NAME)/opt/v2ray: $(TMP)/amd64_$(V_V2RAY)/v2ray | $(VAR)/sh $(TMP)
+	mkdir -v -p -- '$@'
+	cp -v -f -- '$(<D)'/*.dat '$@'
+
+$(TMP)/$(V2RAY_NAME)/DEBIAN/control: ./DEBIAN/control | /usr/bin/envsubst
+	mkdir -v -p -- '$(@D)'
+	ARCH='all' VERSION='$(V_V2RAY)' NAME='v2ray-dat' envsubst <'$<' >'$@'
+
+$(TMP)/$(V2RAY_NAME).deb: $(TMP)/$(V2RAY_NAME)/opt/v2ray $(TMP)/$(V2RAY_NAME)/DEBIAN/control | /usr/bin/debsigs
+	dpkg-deb --root-owner-group --build -- '$(dir $(<D))' '$@'
+	debsigs --sign=archive -- '$@'
+
+PKGS += $(DEB)/$(V2RAY_NAME).deb
+$(DEB)/$(V2RAY_NAME).deb: $(TMP)/$(V2RAY_NAME).deb | $(DEB)
+	cp -v -f -- '$<' '$@'
