fb79e17083ac642c505737722ef4f0057b2e650a
root <github@bigly.dog>
2024-03-19 20:47:50 -0700

    fix

--- makelib/pack-z-push.mk
+++ makelib/pack-z-push.mk
@@ -3,9 +3,9 @@
 V_ZPUSH := $(shell $(GH_LATEST) Z-Hub/Z-Push)
 Z_PUSH_NAME := all_$(V_ZPUSH)_z-push
 
-$(TMP)/$(Z_PUSH_NAME)/opt/z-push: | $(VAR)/sh $(TMP)
+$(TMP)/$(Z_PUSH_NAME)/usr/share/z-push: | $(VAR)/sh $(TMP)
 	mkdir -v -p -- '$@'
-	'$(UNPACK)' 'https://github.com/Z-Hub/Z-Push/archive/refs/tags/$(V_ZPUSH).tar.gz' '$@' --wildcards --strip-components 2 '*/src'
+	'$(UNPACK)' 'https://github.com/Z-Hub/Z-Push/archive/refs/tags/$(V_ZPUSH).tar.gz' '$@' --wildcards --strip-components 2 '*/src' '*/config'
 
 $(TMP)/$(Z_PUSH_NAME)/DEBIAN/control: ./DEBIAN/control | /usr/bin/envsubst
 	mkdir -v -p -- '$(@D)'
@@ -14,7 +14,7 @@ $(TMP)/$(Z_PUSH_NAME)/DEBIAN/control: ./DEBIAN/control | /usr/bin/envsubst
 	Depends: php, php-cli, php-soap, php-mbstring, php-imap, php-fpm
 	EOF
 
-$(TMP)/$(Z_PUSH_NAME).deb: $(TMP)/$(Z_PUSH_NAME)/opt/z-push $(TMP)/$(Z_PUSH_NAME)/DEBIAN/control | /usr/bin/debsigs
+$(TMP)/$(Z_PUSH_NAME).deb: $(TMP)/$(Z_PUSH_NAME)/usr/share/z-push $(TMP)/$(Z_PUSH_NAME)/DEBIAN/control | /usr/bin/debsigs
 	dpkg-deb --root-owner-group --build -- '$(dir $(<D))' '$@'
 	debsigs --sign=archive -- '$@'
 
