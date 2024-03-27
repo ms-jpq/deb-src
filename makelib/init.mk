dd356ba97c09ccc6f5a6f98b1b1c4e366a6049a7
root <github@bigly.dog>
2024-03-27 12:04:00 -0700

    tmux

--- makelib/init.mk
+++ makelib/init.mk
@@ -1,10 +1,19 @@
+.PHONY: init
+
+DPKG_ARCH := $(shell dpkg --print-architecture)
 GH_LATEST := ./libexec/gh-latest.sh $(TMP)
 CURL := curl --fail-with-body --location --no-progress-meter
 UNPACK := $(VAR)/sh/layers/posix/home/.local/opt/initd/libexec/curl-unpack.sh
 
-/usr/bin/envsubst /usr/bin/debsigs /usr/bin/apt-ftparchive /usr/bin/pip:
+APT :=
+APT += gettext-base apt-utils debsigs
+
+init: $(TMP)/.init
+$(TMP)/.init:
 	sudo -- apt-get update
-	sudo -- env -- DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends --yes -- gettext-base apt-utils debsigs
+	sudo -- env -- DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends --yes -- $(APT)
+
+/usr/bin/envsubst /usr/bin/debsigs /usr/bin/apt-ftparchive /usr/bin/pip: | $(TMP)/.init
 
 $(VAR)/sh: | $(VAR)
 	if [[ -d '$@' ]]; then
---
+++ makelib/pack-tmux.mk
@@ -0,0 +1,30 @@
+.PHONY: tmux
+APT += libevent-dev ncurses-dev build-essential bison pkg-config autoconf
+
+V_TMUX := $(shell $(GH_LATEST) tmux/tmux)
+
+/usr/include/event.h: | $(TMP)/.init
+
+$(TMP)/tmux-$(V_TMUX): | $(VAR)/sh $(TMP)
+	'$(UNPACK)' 'https://github.com/tmux/tmux/releases/latest/download/tmux-$(V_TMUX).tar.gz' '$(@D)'
+
+$(TMP)/$(DPKG_ARCH)_$(V_TMUX)_tmux/DEBIAN/control: ./DEBIAN/control | /usr/bin/envsubst
+	mkdir -v -p -- '$(@D)'
+	ARCH='$(DPKG_ARCH)' VERSION='$(V_TMUX)' NAME='tmux' envsubst <'$<' >'$@'
+	tee --append -- '$@' <<-'EOF'
+	Depends: libevent, ncurses
+	EOF
+
+$(TMP)/$(DPKG_ARCH)_$(V_TMUX)_tmux/bin/tmux: $(TMP)/tmux-$(V_TMUX) | /usr/include/event.h
+	env --chdir '$<' -- './configure' --prefix='$(abspath $(dir $(@D)))'
+	'$(MAKE)' --directory='$<'
+	'$(MAKE)' --directory='$<' install
+
+$(TMP)/$(DPKG_ARCH)_$(V_TMUX)_tmux.deb: $(TMP)/$(DPKG_ARCH)_$(V_TMUX)_tmux/bin/tmux $(TMP)/$(DPKG_ARCH)_$(V_TMUX)_tmux/DEBIAN/control | /usr/bin/debsigs
+	dpkg-deb --root-owner-group --build -- '$(dir $(<D))' '$@'
+	debsigs --sign=archive -- '$@'
+
+tmux: $(DEB)/$(DPKG_ARCH)_$(V_TMUX)_tmux.deb
+PKGS += $(DEB)/$(DPKG_ARCH)_$(V_TMUX)_tmux.deb
+$(DEB)/$(DPKG_ARCH)_$(V_TMUX)_tmux.deb: $(TMP)/$(DPKG_ARCH)_$(V_TMUX)_tmux.deb | $(DEB)
+	cp -v -f -- '$<' '$@'
