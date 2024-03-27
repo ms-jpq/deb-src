b36933f6b5ae9576d7268e326c41047780b69b8c
root <github@bigly.dog>
2024-03-27 12:39:46 -0700

    sixel

--- makelib/pack-tmux.mk
+++ makelib/pack-tmux.mk
@@ -1,5 +1,5 @@
 .PHONY: tmux
-APT += libevent-dev ncurses-dev build-essential bison pkg-config autoconf
+APT += libevent-dev ncurses-dev build-essential bison pkg-config libutempter0 libutf8proc-dev autoconf autotools-dev
 
 V_TMUX := $(shell $(GH_LATEST) tmux/tmux)
 
@@ -11,12 +11,9 @@ $(TMP)/tmux-$(V_TMUX): | $(VAR)/sh $(TMP)
 $(TMP)/$(DPKG_ARCH)_$(V_TMUX)_tmux/DEBIAN/control: ./DEBIAN/control | /usr/bin/envsubst
 	mkdir -v -p -- '$(@D)'
 	ARCH='$(DPKG_ARCH)' VERSION='$(V_TMUX)' NAME='tmux' envsubst <'$<' >'$@'
-	tee --append -- '$@' <<-'EOF'
-	Depends: libc6, libtinfo6, libutempter0
-	EOF
 
 $(TMP)/$(DPKG_ARCH)_$(V_TMUX)_tmux/bin/tmux: $(TMP)/tmux-$(V_TMUX) | /usr/include/event.h
-	env --chdir '$<' -- './configure' --prefix='$(abspath $(dir $(@D)))'
+	env --chdir '$<' -- './configure' --prefix='$(abspath $(dir $(@D)))' --enable-static --enable-utf8proc --enable-sixel
 	'$(MAKE)' --directory='$<'
 	'$(MAKE)' --directory='$<' install
 
