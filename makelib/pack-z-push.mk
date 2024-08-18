c476c4985da0dc29927d1e27cf555232bfbc0bb6
hao <hao@stainlessapi.com>
2024-08-17 22:17:46 -0400

    fix name

--- makelib/pack-z-push.mk
+++ makelib/pack-z-push.mk
@@ -1,25 +1,25 @@
 .PHONY: zpush
 
 V_ZPUSH := $(shell $(GH_LATEST) Z-Hub/Z-Push)
-Z_PUSH_NAME := all_$(V_ZPUSH)_z-push
+ZPUSH_NAME := all_$(V_ZPUSH)_z-push
 
-$(TMP)/$(Z_PUSH_NAME)/usr/share/z-push: | $(VAR)/sh $(TMP)
+$(TMP)/$(ZPUSH_NAME)/usr/share/z-push: | $(VAR)/sh $(TMP)
 	mkdir -v -p -- '$@'
 	'$(UNPACK)' 'https://github.com/Z-Hub/Z-Push/archive/refs/tags/$(V_ZPUSH).tar.gz' '$@' --wildcards --strip-components 2 '*/src' '*/config'
 
-$(TMP)/$(Z_PUSH_NAME)/DEBIAN/control: ./DEBIAN/control | /usr/bin/envsubst
+$(TMP)/$(ZPUSH_NAME)/DEBIAN/control: ./DEBIAN/control | /usr/bin/envsubst
 	mkdir -v -p -- '$(@D)'
 	ARCH='all' VERSION='$(V_ZPUSH)' NAME='z-push' envsubst <'$<' >'$@'
 	tee --append -- '$@' <<-'EOF'
 	Depends: php, php-cli, php-soap, php-mbstring, php-imap
 	EOF
 
-$(TMP)/$(Z_PUSH_NAME).deb: $(TMP)/$(Z_PUSH_NAME)/usr/share/z-push $(TMP)/$(Z_PUSH_NAME)/DEBIAN/control | /usr/bin/debsigs
+$(TMP)/$(ZPUSH_NAME).deb: $(TMP)/$(ZPUSH_NAME)/usr/share/z-push $(TMP)/$(ZPUSH_NAME)/DEBIAN/control | /usr/bin/debsigs
 	DIR='$(dir $(<D))'
 	dpkg-deb --root-owner-group --build -- "$${DIR%/*/*}" '$@'
 	debsigs --sign=archive -- '$@'
 
-PKGS += $(DEB)/$(Z_PUSH_NAME).deb
-zpush: $(DEB)/$(Z_PUSH_NAME).deb
-$(DEB)/$(Z_PUSH_NAME).deb: $(TMP)/$(Z_PUSH_NAME).deb | $(DEB)
+PKGS += $(DEB)/$(ZPUSH_NAME).deb
+zpush: $(DEB)/$(ZPUSH_NAME).deb
+$(DEB)/$(ZPUSH_NAME).deb: $(TMP)/$(ZPUSH_NAME).deb | $(DEB)
 	cp -v -f -- '$<' '$@'
