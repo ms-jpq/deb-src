5ded127c0135717f0d4c77378aaac7f72e504f17
hao <hao@stainlessapi.com>
2024-10-02 12:29:21 -0400

    tag

--- libexec/gh-latest.sh
+++ libexec/gh-latest.sh
@@ -9,16 +9,16 @@ CACHE="$TMP/$NAME.cache"
 
 mkdir -v -p -- "$TMP" >&2
 
-if ! [[ -v LOCKED ]] && command -v -- flock >/dev/null; then
+if ! [[ -v LOCKED ]] && command -v -- flock > /dev/null; then
   LOCK="$TMP/$NAME.lock"
   LOCKED=1 exec -- flock "$LOCK" "$0" "$@"
 fi
 
-if [[ -f "$CACHE" ]]; then
+if [[ -f $CACHE ]]; then
   find "$CACHE" -type f -mmin '+60' -delete
 fi
 
-if ! [[ -f "$CACHE" ]]; then
+if ! [[ -f $CACHE ]]; then
   CURL=(
     curl
     --fail-with-body
@@ -29,18 +29,27 @@ if ! [[ -f "$CACHE" ]]; then
   if [[ -v GH_TOKEN ]]; then
     CURL+=(--oauth2-bearer "$GH_TOKEN")
   fi
-  CURL+=(
-    -- "https://api.github.com/repos/$REPO/releases/latest"
-  )
+
+  if [[ -v LATEST_TAG ]]; then
+    CURL+=(-- "https://api.github.com/repos/$REPO/tags")
+  else
+    CURL+=(-- "https://api.github.com/repos/$REPO/releases/latest")
+  fi
 
   JQ=(
     jq --exit-status
-    --raw-output '.tag_name'
+    --raw-output
   )
 
+  if [[ -v LATEST_TAG ]]; then
+    JQ+=('.[0].name')
+  else
+    JQ+=('.tag_name')
+  fi
+
   LINES="$("${CURL[@]}" | "${JQ[@]}")"
-  readarray -t -- TAGS <<<"$LINES"
-  printf -- '%s' "${TAGS[0]}" >"$CACHE"
+  readarray -t -- TAGS <<< "$LINES"
+  printf -- '%s' "${TAGS[0]}" > "$CACHE"
 fi
 
 exec -- cat -- "$CACHE"
