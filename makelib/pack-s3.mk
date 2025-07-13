d7e92a85d3667b2e4b9d5dac7fa2e14eb6d88811
dogisgreat <git@bigly.dog>
2025-07-13 14:07:39 -0400

    ollama

--- makelib/pack-s3.mk
+++ makelib/pack-s3.mk
@@ -1,3 +1,5 @@
+.PHONY: ollama tabby
+
 S3 := $(VAR)/s3
 
 $(S3): | $(VAR)
@@ -5,3 +7,14 @@ $(S3): | $(VAR)
 
 V_OLLAMA := $(patsubst v%,%,$(shell $(GH_LATEST) ollama/ollama))
 V_TABBY  := $(patsubst v%,%,$(shell $(GH_LATEST) TabbyML/tabby))
+
+OLLAMA_URL := https://github.com/ollama/ollama/releases/latest/download/ollama-linux-amd64.tgz
+TABBY_URL  := https://github.com/TabbyML/tabby/releases/latest/download/tabby_x86_64-manylinux_2_28-cuda123.tar.gz
+
+OLLAMA_SHORT := amd64_$(V_OLLAMA)
+TABBY_SHORT  := amd64_$(V_TABBY)
+
+$(TMP)/$(OLLAMA_SHORT): | $(VAR)/sh $(TMP)
+	'$(UNPACK)' '$(OLLAMA_URL)' '$@'
+
+ollama: $(TMP)/$(OLLAMA_NAME)
